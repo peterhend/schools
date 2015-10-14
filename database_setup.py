549919d65614117ed46239373091bca88f9e03ee
Peter Henderson <git@hvdoc.com>
2015-10-14 11:53:20 -0400

    Update

--- database_setup.py
+++ database_setup.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
-Base = declarative_base()
+Base = declarative_base() 
 
 class District(Base):
     __tablename__ = 'district'
